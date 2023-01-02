64eef519e0d14221b3439cfa31b407a782eef5ca
CAPNBRAD <119074058+CAPNBRAD@users.noreply.github.com>
2023-01-02 12:06:42 -0800

    added Basic JavaScript Project folder
    Added Javascript files and projects, window.alert, multiple variable assignment, document.write, and functions

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,31 @@
+var A = "By clicking this box, You agree to give me a passing grade!"
+
+var B="cowboy" + "action"
+
+var family="The Painters", Dad="Brad",Mom="Amanda",Son="Maddox",Cat="Cheeze",Dog="Terra";   //these define variables, should come before main code
+
+var Sent1="<br>This is the beginning of the string"
+
+var Sent2="and this is the end of the string"
+
+function My_First_Function()    {       //This defines and names a function
+    var str = "This is the button!";    //defines the variable, gives string value
+    document.getElementById("Button_Text").innerHTML = str;     //puts the value of variable into html element
+}
+
+
+window.alert(A);
+
+document.write("<br>if i gave you money would you pass me?<br> \"heck no try harder.\"<br>"); //showing hidden/unhidden characters
+
+document.write("Perhaps you have a pomegranite?<br>"
++"No actually, I buy them fresh daily"
++"if you want one, go to the store<br>");   //string concatenation
+
+
+document.write(B)
+document.write(Son);
+document.write(5+7);    // expression
+document.write(Sent1+Sent2);
+
+
